460212d24d6c53099027bbf212f676b2af4c2f9f
Sudip Bhusal <pidusbhusal@gmail.com>
2025-02-01 14:55:49 -0500

    quiz checking correct answer animation solved

--- app/components/quizCard.tsx
+++ app/components/quizCard.tsx
@@ -12,11 +12,18 @@ export default function QuizCard({
   CorrectAns,
 }: quizCardType) {
   return (
-    <div>
-      <h3>{Question}</h3>
-      <div>
+    <div className="container mx-auto bg-white shadow-xl rounded-2xl p-6 py-20 border border-gray-200 transition-transform transform ">
+      <h3 className="text-xl font-semibold text-gray-800 mb-4 text-center">
+        {Question}
+      </h3>
+      <div className="grid gap-8">
         {Answers.map((answer: string, key: number) => (
-          <Qbtn option={answer} correctAnswer={CorrectAns} index={key} />
+          <Qbtn
+            key={key}
+            option={answer}
+            correctAnswer={CorrectAns}
+            index={key}
+          />
         ))}
       </div>
     </div>
--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -2,24 +2,35 @@ import Qbtn from "@/app/components/buttons/quizbtn";
 import { fetchQuiz } from "@/lib/api";
 import QuizCard from "@/app/components/quizCard";
 
-type QuizType = {};
+type quizeType = {
+  question: string;
+  comment: string;
+  test_answer: number;
+
+  answers: string[];
+};
+type quizesType = {
+  question: quizeType[];
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   try {
     const data = await fetchQuiz(params.id);
     const Quizes = data.test.question;
     return (
-      <h1>
-        {Quizes.map((quiz: any) => (
+      <div>
+        {Quizes.map((quiz: quizeType, index: number) => (
           <QuizCard
+            key={index}
             Question={quiz.question}
             CorrectAns={quiz.test_answer}
             Answers={quiz.answers}
           />
         ))}
-      </h1>
+      </div>
     );
   } catch (error) {
     console.log(error);
+    return <p>Error Loading Quiz</p>;
   }
 }
